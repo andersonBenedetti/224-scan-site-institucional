b62b126b7d73c73b591e97ca4310f76ad0f2de8d
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2025-06-09 12:35:54 -0300

    feat: add tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,3 +83,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   createDots();
 });
+
+// TABS
+const buttons = document.querySelectorAll(".tab-btn");
+const panes = document.querySelectorAll(".tab-pane");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    buttons.forEach((b) => b.classList.remove("active"));
+    panes.forEach((p) => p.classList.remove("active"));
+
+    btn.classList.add("active");
+    document.getElementById(btn.dataset.tab).classList.add("active");
+  });
+});
